0745c5d7da4cfa45ee473e9014b9fae56f42f166
aQua <6028869+aQuaYi@users.noreply.github.com>
2019-04-01 10:00:25 +0800

    flayweight 修改完毕

--- DesignPatterns/flyweight/main.go
+++ DesignPatterns/flyweight/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	bs := newBigString("110")
 	fmt.Println(bs)
+
+	// 轻量的秘密在于共享数据
+	// 所以，修改 bs[0] 后，bs[1] 中的 "1" 也被修改了。
+	one := bs[0]
+	one.fontData = strings.Replace(one.fontData, ".", "`", -1)
+	fmt.Println(bs)
+
 }
